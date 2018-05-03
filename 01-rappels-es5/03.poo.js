c87e3fa0fcd265e47962b2798e6ac2bac9da0370
Maxime.DANIEL <m.daniel2009@hotmail.fr>
2018-05-03 13:59:12 +0200

    objet

---
+++ 01-rappels-es5/03.poo.js
@@ -0,0 +1,32 @@
+function Personne(nom,prenom,pseudo){
+    this.nom = nom,
+    this.prenom = prenom,
+    this.pseudo = pseudo,
+    this.getNomcomplet = function(){
+        return  nom + prenom + pseudo;
+    },
+    this.getInitiales = function(){
+        return  nom.charAt(0) + prenom.charAt(0) ;
+    };
+}
+jules = new Personne("LEMAIRE","Jules","jules77");
+paul = new Personne("LEMAIRE","Paul","paul44");
+
+console.log(jules.nom);
+console.log(jules.prenom);
+console.log(jules.getNomcomplet());
+
+jules.pseudo = "jules44";
+
+console.log(jules.getNomcomplet());
+console.log(jules.age);
+
+Personne.prototype.age;
+console.log(jules.age);
+
+jules.age = 30;
+
+console.log(jules.age);
+
+console.log(jules.getInitiales());
+
